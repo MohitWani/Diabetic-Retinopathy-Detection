0304c5e7b8ac565ee1d9789ba1a27e945cca5ab7
MohitWani <wanimohit1@gmail.com>
2024-02-26 19:39:50 +0530

    model.py added

--- Diabetic_Retinopathy/detection.py
+++ Diabetic_Retinopathy/detection.py
@@ -1,9 +1,19 @@
-import tensorflow as tf
 from tensorflow.keras.preprocessing import image
+import numpy as np
 
 
-def detection(img_path):
-    model = tf.keras.models.load_model('retina_weights.hdf5')
-    img = image.load_img(img_path, target_size=(224, 224))
-    prediction = model.predict(img)
-    return prediction
+def load_weight(model, weights_path):
+    model.load_weights(weights_path)
+
+def preprocess_image(image_path):
+    img = image.load_img(image_path, target_size=(256, 256))
+    img_array = image.img_to_array(img)
+    img = img_array / 255
+    img = img.reshape(-1,256,256,3)
+    return img
+
+
+def detection(model, img_array):
+    prediction = model.predict(img_array)
+    predict = np.argmax(prediction)
+    return predict
---
+++ Diabetic_Retinopathy/model.py
@@ -0,0 +1,121 @@
+from tensorflow.keras.layers import *
+from tensorflow.keras.initializers import glorot_uniform
+from tensorflow.keras.models import Model
+
+
+
+def res_block(X, filter, stage):
+  
+  # Convolutional_block
+  X_copy = X
+
+  f1 , f2, f3 = filter
+    
+  # Main Path
+  X = Conv2D(f1, (1,1),strides = (1,1), name ='res_'+str(stage)+'_conv_a', kernel_initializer= glorot_uniform(seed = 0))(X)
+  X = MaxPool2D((2,2))(X)
+  X = BatchNormalization(axis =3, name = 'bn_'+str(stage)+'_conv_a')(X)
+  X = Activation('relu')(X) 
+
+  X = Conv2D(f2, kernel_size = (3,3), strides =(1,1), padding = 'same', name ='res_'+str(stage)+'_conv_b', kernel_initializer= glorot_uniform(seed = 0))(X)
+  X = BatchNormalization(axis =3, name = 'bn_'+str(stage)+'_conv_b')(X)
+  X = Activation('relu')(X) 
+
+  X = Conv2D(f3, kernel_size = (1,1), strides =(1,1),name ='res_'+str(stage)+'_conv_c', kernel_initializer= glorot_uniform(seed = 0))(X)
+  X = BatchNormalization(axis =3, name = 'bn_'+str(stage)+'_conv_c')(X)
+
+
+  # Short path
+  X_copy = Conv2D(f3, kernel_size = (1,1), strides =(1,1),name ='res_'+str(stage)+'_conv_copy', kernel_initializer= glorot_uniform(seed = 0))(X_copy)
+  X_copy = MaxPool2D((2,2))(X_copy)
+  X_copy = BatchNormalization(axis =3, name = 'bn_'+str(stage)+'_conv_copy')(X_copy)
+
+  # ADD
+  X = Add()([X,X_copy])
+  X = Activation('relu')(X)
+
+  # Identity Block 1
+  X_copy = X
+
+
+  # Main Path
+  X = Conv2D(f1, (1,1),strides = (1,1), name ='res_'+str(stage)+'_identity_1_a', kernel_initializer= glorot_uniform(seed = 0))(X)
+  X = BatchNormalization(axis =3, name = 'bn_'+str(stage)+'_identity_1_a')(X)
+  X = Activation('relu')(X) 
+
+  X = Conv2D(f2, kernel_size = (3,3), strides =(1,1), padding = 'same', name ='res_'+str(stage)+'_identity_1_b', kernel_initializer= glorot_uniform(seed = 0))(X)
+  X = BatchNormalization(axis =3, name = 'bn_'+str(stage)+'_identity_1_b')(X)
+  X = Activation('relu')(X) 
+
+  X = Conv2D(f3, kernel_size = (1,1), strides =(1,1),name ='res_'+str(stage)+'_identity_1_c', kernel_initializer= glorot_uniform(seed = 0))(X)
+  X = BatchNormalization(axis =3, name = 'bn_'+str(stage)+'_identity_1_c')(X)
+
+  # ADD
+  X = Add()([X,X_copy])
+  X = Activation('relu')(X)
+
+  # Identity Block 2
+  X_copy = X
+
+
+  # Main Path
+  X = Conv2D(f1, (1,1),strides = (1,1), name ='res_'+str(stage)+'_identity_2_a', kernel_initializer= glorot_uniform(seed = 0))(X)
+  X = BatchNormalization(axis =3, name = 'bn_'+str(stage)+'_identity_2_a')(X)
+  X = Activation('relu')(X) 
+
+  X = Conv2D(f2, kernel_size = (3,3), strides =(1,1), padding = 'same', name ='res_'+str(stage)+'_identity_2_b', kernel_initializer= glorot_uniform(seed = 0))(X)
+  X = BatchNormalization(axis =3, name = 'bn_'+str(stage)+'_identity_2_b')(X)
+  X = Activation('relu')(X) 
+
+  X = Conv2D(f3, kernel_size = (1,1), strides =(1,1),name ='res_'+str(stage)+'_identity_2_c', kernel_initializer= glorot_uniform(seed = 0))(X)
+  X = BatchNormalization(axis =3, name = 'bn_'+str(stage)+'_identity_2_c')(X)
+
+  # ADD
+  X = Add()([X,X_copy])
+  X = Activation('relu')(X)
+
+  return X
+
+def build_model():
+    input_shape = (256,256,3)
+    X_input = Input(input_shape)
+
+    #Zero-padding
+
+    X = ZeroPadding2D((3,3))(X_input)
+
+    # 1 - stage
+
+    X = Conv2D(64, (7,7), strides= (2,2), name = 'conv1', kernel_initializer= glorot_uniform(seed = 0))(X)
+    X = BatchNormalization(axis =3, name = 'bn_conv1')(X)
+    X = Activation('relu')(X)
+    X = MaxPooling2D((3,3), strides= (2,2))(X)
+
+    # 2- stage
+
+    X = res_block(X, filter= [64,64,256], stage= 2)
+
+    # 3- stage
+
+    X = res_block(X, filter= [128,128,512], stage= 3)
+
+    # 4- stage
+
+    X = res_block(X, filter= [256,256,1024], stage= 4)
+
+    # # 5- stage
+
+    X = res_block(X, filter= [512,512,2048], stage= 5)
+
+    #Average Pooling
+
+    X = AveragePooling2D((2,2), name = 'Averagea_Pooling')(X)
+
+    #Final layer
+
+    X = Flatten()(X)
+    X = Dense(5, activation = 'softmax', name = 'Dense_final', kernel_initializer= glorot_uniform(seed=0))(X)
+
+
+    model = Model( inputs= X_input, outputs = X, name = 'Resnet18')
+    return model
